a1c310691235b8dd84dd6083332a62128486c8eb
Noteworthy Software, Inc <online@noteworthysoftware.com>
2016-09-26 14:11:21 -0400

    NWC 2 Help File from Feb 2010

--- helptips.php
+++ helptips.php
@@ -1,20 +1,22 @@
-<?php
+<? ob_start("ob_gzhandler");
 
-if (empty($_GET['id'])) help_output("No id requested.");
+if (!isset($_GET['id'])) help_output("No id requested.");
 $id = $_GET['id'];
 
+//$data = file("$_SERVER[DOCUMENT_ROOT]/nwchelp/helptips.txt");
 $data = file("helptips.txt");
 $running_key = "";
 $help = array();
 
 foreach($data as $s) {
-	if (preg_match('/^\|([a-zA-Z0-9\_]+)\|/',$s,$m)) {
-		$running_key = $m[1];
-		$help[$running_key] = "";
-	} else if ($running_key) {
-		$help[$running_key] .= $s;
-	}
-}
+  if (preg_match('/^\|([a-zA-Z0-9\_]+)\|/',$s,$m)) {
+    $running_key = $m[1];
+    $help[$running_key] = "";
+    }
+  else if ($running_key) {
+    $help[$running_key] .= $s;
+    }
+  }
 
 if (isset($help[$id])) help_output($help[$id]);
 
@@ -23,8 +25,10 @@ help_output($help['tip_unassigned']);
 function help_output($s)
 {
 ?><html><head><title>NoteWorthy Composer Help Tips</title></head>
-<body><?php echo $s?></body>
+<body><?=$s?></body>
 </html>
-<?php
-	exit;
+<?
+  exit;
 }
+
+?>
